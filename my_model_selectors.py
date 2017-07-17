6c13cb15b7004994c481686011897a296dbedc78
Alexey Sas <asasryl@gmail.com>
2017-07-16 23:35:12 -0500

    some stuff

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -28,8 +28,36 @@ class ModelSelector(object):
         self.random_state = random_state
         self.verbose = verbose
 
-    def select(self):
+    def build_model(self, n_states):
+        '''
+        Abstract method to fit model and calculate score,
+        so it can be compared with other models by 'select' method
+        '''
         raise NotImplementedError
+
+    def select(self):
+        '''
+        Enumerate possible parametrs (number of states)
+        and select model with minimum error
+        '''
+        warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+        #set default model in case we always fail
+        best_num_components = self.n_constant
+        best_model = self.base_model(best_num_components)
+        best_score = float("inf")
+
+        for n_states in range(self.min_n_components, self.max_n_components + 1):
+            try:
+                model, score = self.build_model(n_states)
+                if score < best_score:
+                    best_score = score
+                    best_model = model
+                    best_num_components = n_states
+            except BaseException:
+                print("Failed to build model woth n_states={}".format(n_states))
+
+        return best_model
 
     def base_model(self, num_states):
         # with warnings.catch_warnings():
@@ -68,16 +96,52 @@ class SelectorBIC(ModelSelector):
     Bayesian information criteria: BIC = -2 * logL + p * logN
     """
 
-    def select(self):
-        """ select the best model for self.this_word based on
-        BIC score for n between self.min_n_components and self.max_n_components
+    def build_model(self, n_states):
+        '''
+        Select best model based on BIC:
+        https://rdrr.io/cran/HMMpa/man/AIC_HMM.html
 
-        :return: GaussianHMM object
-        """
-        warnings.filterwarnings("ignore", category=DeprecationWarning)
+        BIC = -2 logL + p log(N)
+        N is the number of data points
+        p is the number of parameters
+        L is the likelihood of the fitted model
 
-        # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        '''
+
+        # train model
+        model = self.base_model(n_states)
+        model = model.fit(self.X, self.lengths)
+
+        # calculate log-liklyhood for the model
+        log_l_score = model.score(self.X, self.lengths)
+
+        # calculate N
+        N = np.mean(self.lengths)
+        # not exactly sure if we need mean or sum.
+        # probably as soon as we are consistent with this either might work
+
+        # calculate p
+        # to calculate p we need to sum up all parameters for the model which are trainables
+        # according to the links trainables are: model.startprob_,
+        # model.transmat_, model.covars_, model.means
+        # http://hmmlearn.readthedocs.io/en/latest/api.html#hmmlearn-hmm
+        # https://github.com/hmmlearn/hmmlearn/blob/master/hmmlearn/hmm.py
+        # as sums of rows are equal to 1 so last parameter is calculated
+        # and can not be considered as trainable
+        p = np.size(model.transmat_) - np.size(model.transmat_, 0)
+
+        # again sum should be equal to 1 so substracting one value
+        p += np.size(model.startprob_) - 1
+
+        # for the covariance_type="diag" only diagonal elements should be considered
+        # so instead of np.size(model.covars_) going to use np.size(model.means_)
+        # as using np.size(model.covars_) will lead to incorrect results.
+        p += np.size(model.means_) + np.size(model.means_)
+
+        # combine score
+        score = -2 * log_l_score + p * np.log(N)
+
+        return model, score
 
 
 class SelectorDIC(ModelSelector):
@@ -97,12 +161,36 @@ class SelectorDIC(ModelSelector):
 
 
 class SelectorCV(ModelSelector):
-    ''' select best model based on average log Likelihood of cross-validation folds
+    '''select best model based on average log Likelihood of cross-validation folds
 
     '''
 
-    def select(self):
-        warnings.filterwarnings("ignore", category=DeprecationWarning)
+    def build_model(self, n_states):
+        '''
+        select best model based on average log Likelihood of cross-validation folds
+        '''
 
-        # TODO implement model selection using CV
-        raise NotImplementedError
+        split_method = KFold()
+        scores = []
+
+        # enumerate index sets
+        for cv_train_idx, cv_test_idx in split_method.split(self.sequences):
+
+            # prepare training and test data
+            train_set_data, train_set_lengths = combine_sequences(cv_train_idx, self.sequences)
+            test_set_data, test_set_lengths = combine_sequences(cv_test_idx, self.sequences)
+
+            # train model
+            model = self.base_model(n_states)
+            model = model.fit(train_set_data, train_set_lengths)
+
+            # calculate log-liklyhood for the model
+            log_l_score = model.score(test_set_data, test_set_lengths)
+
+            # collect score to 
+            scores.append(log_l_score)
+
+        print(np.mean(scores))
+        return model, np.mean(scores)
+
+
